2cd86ecd61979157487d92d6bde15480e21fb783
Alan Kligman <alan.kligman@gmail.com>
2012-05-16 11:34:37 -0400

    Fixed a bunch of lint errors. More model implementation.

--- src/components/model.js
+++ src/components/model.js
@@ -8,13 +8,17 @@ define( function( require ) {
   var Component = require( "base/component" );
 
   var Model = function( service, mesh, materialDefinition ) {
-
+    this.mesh = mesh || new service.target.context.Mesh();
+    this.materialDefinition = materialDefinition || 
+      new service.target.context.Material();
   };
   Model.prototype = new Component();
   Model.prototype.constructor = Model;
 
   function onUpdate( event ) {
-
+    if( this.mesh && this.materialDefinition ) {
+      
+    }
   }
 
   function onEntitySpaceChanged( event ) {
