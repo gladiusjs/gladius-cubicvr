c0551f5c0acee058ff02cb83d84927758dbad426
David Perit <dperit@gmail.com>
2012-05-16 12:50:52 -0400

    Reworked light to add prototypes and event handlers along the lines of how model works.

--- src/components/light.js
+++ src/components/light.js
@@ -4,10 +4,28 @@ if ( typeof define !== "function" ) {
 
 define(
   [ "src/resources/light-definition",
-    "lib/_math"],
-  function ( LightDefinition, Math ) {
+    "lib/_math",
+    "base/component",
+    "common/extend" ],
+  function ( LightDefinition, math, Component, extend ) {
+
+    //Assign all of these values to the cubicVRLight
+    var properties = [
+      "light_type",
+      "diffuse",
+      "specular",
+      "intensity",
+      "distance",
+      "cutoff",
+      "map_res",
+      "method",
+      "areaCeiling",
+      "areaFloor",
+      "areaAxis"
+    ];
 
     var Light = function( service, lightDefinition ){
+      Component.call(this, "Light", service, ["Transform"]);
 
       if (!(lightDefinition instanceof LightDefinition)){
         lightDefinition = new LightDefinition();
@@ -15,55 +33,61 @@ define(
 
       this._cubicVRLight = new service.target.context.Light(lightDefinition);
 
-      this.definition = lightDefinition;
-
-      //Assign all of these values to the cubicVRLight
-      var properties = [
-        "light_type",
-        "diffuse",
-        "specular",
-        "intensity",
-        "distance",
-        "cutoff",
-        "map_res",
-        "method",
-        "areaCeiling",
-        "areaFloor",
-        "areaAxis"
-      ];
-
-      //If no value has been assigned to this specific light for the given property name then return the one from lightDefinition instead
-      var defineProperty = function(propertyName, that){
-        Object.defineProperty(that, propertyName, {
-          get: function() {
-            if (that.hasOwnProperty("_" + propertyName)){
-              return that["_" + propertyName];
-            }else{
-              return that.definition[propertyName];
-            }
-          },
-          //I would have caused this to overwrite the setters/getters with something that just returns that[propertyName]
-          //but there doesn't appear to be a way to reassign setters/getters for an object, at least not from within the
-          //setter
-          set: function(value) {
-            that["_" + propertyName] = value;
-            //Set the corresponding property in cubicVRLight
-            that._cubicVRLight[propertyName] = value;
-          }
-        });
-      };
-
       for (var propertyIndex = 0; propertyIndex < properties.length; propertyIndex++){
-        defineProperty(properties[propertyIndex], this);
+        this[properties[propertyIndex]] = lightDefinition[properties[propertyIndex]];
       }
 
-      this.onUpdate = function(){
-        //This should return the absolute light position
-        this._cubicVRLight.position = Math.matrix4.multiplyVector3(this.owner.findComponent( "Transform" ).absolute(), [ 0, 0, 0]);
-        //this._cubicVRLight.rotation = this.owner.findComponent( "Rotation" ).absolute();
-      };
-
     };
+    Light.prototype = new Component();
+    Light.prototype.constructor = Light;
+
+    function onUpdate(event){
+      for (var propertyIndex = 0; propertyIndex < properties.length; propertyIndex++){
+        this._cubicVRLight[properties[propertyIndex]] = this[properties[propertyIndex]];
+      }
+      //This should return the absolute light position
+      this._cubicVRLight.position = math.matrix4.multiplyVector3(this.owner.findComponent( "Transform" ).absolute(), [ 0, 0, 0]);
+      //this._cubicVRLight.rotation = this.owner.findComponent( "Rotation" ).absolute();
+    }
+
+    function onEntitySpaceChanged( event ) {
+      var data = event.data;
+      if( data.previous === null && data.current !== null && this.owner !== null ) {
+        this.provider.registerComponent( this.owner.id, this );
+      }
+
+      if( data.previous !== null && data.current === null && this.owner !== null ) {
+        this.provider.unregisterComponent( this.owner.id, this );
+      }
+    }
+
+    function onComponentOwnerChanged( event ) {
+      var data = event.data;
+      if( data.previous === null && this.owner !== null ) {
+        this.provider.registerComponent( this.owner.id, this );
+      }
+
+      if( this.owner === null && data.previous !== null ) {
+        this.provider.unregisterComponent( data.previous.id, this );
+      }
+    }
+
+    function onEntityActivationChanged( event ) {
+      var active = event.data;
+      if( active ) {
+        this.provider.registerComponent( this.owner.id, this );
+      } else {
+        this.provider.unregisterComponent( this.owner.id, this );
+      }
+    }
+
+    var prototype = {
+      onUpdate: onUpdate,
+      onEntitySpaceChanged: onEntitySpaceChanged,
+      onComponentOwnerChanged: onComponentOwnerChanged,
+      onEntityActivationChanged: onEntityActivationChanged
+    };
+    extend( Light.prototype, prototype );
 
     return Light;
 
