a3396803cdc655e0f6d2bf19db1c1a1141611e4d
Alan Kligman <alan.kligman@gmail.com>
2012-05-17 11:58:40 -0400

    Fixed a bug in mesh where it wasn't being compiled.

--- src/resources/mesh.js
+++ src/resources/mesh.js
@@ -7,6 +7,7 @@ define( function ( require ) {
   var Mesh = function( service, data ) {
     var mesh = new service.target.context.Mesh( data );
     mesh._gladius = {};
+    mesh.prepare();
     return mesh;
   };
 
